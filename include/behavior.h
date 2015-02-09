e57a799aa8de3e78020b9ae3ef2eef0eef2a02f4
Luke Fraser <lukefrasera@gmail.com>
2015-02-08 16:31:23 -0800

    adding behavior classes and generating Node implementation file.

---
+++ include/behavior.h
@@ -0,0 +1,36 @@
+/*
+robotics-task-tree-eval
+Copyright (C) 2015  Luke Fraser
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef INCLUDE_BEHAVIOR_H_
+#define INCLUDE_BEHAVIOR_H_
+
+#include "node.h"
+namespace task_net {
+class Behavior: public Node {
+ public:
+  Behavior();
+  virtual ~Behavior();
+
+ private:
+};
+
+class ThenBehavior: public Behavior {};
+class AndBehavior: public Behavior {};
+class OrBehavior: public Behavior {};
+class WhileBehavior: public Behavior {};
+}
+#endif  // INCLUDE_BEHAVIOR_H_
